33e62b653654ef2cda5b7caa9c5d60327944f4e8
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-07-26 23:35:52 +0500

    learning backend

--- backened/node js/script.js
+++ backened/node js/script.js
@@ -1,10 +1,28 @@
-// var d=require('./script2.js');
+// // var d=require('./script2.js');
+
+// // var a=2;
+// // var b=6;
+// // var c=8;
 
 // var a=2;
 // var b=6;
-// var c=8;
+// // console.log(a+b);
 
-var a=2;
-var b=6;
+// module.exports={first:a,second:b};
 
-modules.exports={first:a,second:b};
+//express
+
+const express=require('express');
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log("this is middleware");
+    next();
+})
+app.get("/",(req,res)=>{
+    res.send("this is home page");
+})
+app.get("/profile",(req,res)=>{
+    res.send("this is profile page");
+})
+app.listen(3000);
--- backened/node js/script2.js
+++ backened/node js/script2.js
@@ -1,3 +1,4 @@
-// var d=10;
+var d=10;
 // modules.exports=d;
 var data=require('./script.js');
+console.log(data.first+data.second+d);
