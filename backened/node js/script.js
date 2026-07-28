59bb2fb23dd6bb7b27e0790edc05f31a931c69c8
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-07-28 15:47:36 +0500

    learning backemnd

--- backened/node js/script.js
+++ backened/node js/script.js
@@ -1,28 +1,37 @@
-// // var d=require('./script2.js');
+// // // var d=require('./script2.js');
+
+// // // var a=2;
+// // // var b=6;
+// // // var c=8;
 
 // // var a=2;
 // // var b=6;
-// // var c=8;
+// // // console.log(a+b);
 
-// var a=2;
-// var b=6;
-// // console.log(a+b);
+// // module.exports={first:a,second:b};
 
-// module.exports={first:a,second:b};
+// //express
 
-//express
+// const express=require('express');
+// const app=express();
 
-const express=require('express');
+// app.use((req,res,next)=>{
+//     console.log("this is middleware");
+//     next();
+// })
+// app.get("/",(req,res)=>{
+//     res.send("this is home page");
+// })
+// app.get("/profile",(req,res)=>{
+//     res.send("this is profile page");
+// })
+// app.listen(3000);
+
+const express =require('express');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("this is middleware");
-    next();
-})
 app.get("/",(req,res)=>{
-    res.send("this is home page");
-})
-app.get("/profile",(req,res)=>{
-    res.send("this is profile page");
+    res.send("thus is home");
+    
 })
 app.listen(3000);
