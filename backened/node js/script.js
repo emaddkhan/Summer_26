6fd7b83ca1361c060f1f65a86bee4c9bf5147f25
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-07-29 16:01:42 +0500

    learned middle ware routing error handling in express js

--- backened/node js/script.js
+++ backened/node js/script.js
@@ -30,8 +30,20 @@
 const express =require('express');
 const app=express();
 
+app.use((req,res,next)=>{
+    console.log("this is middleware");
+    next(); // iska matlab middleware ne aage req bheji route ko
+})
 app.get("/",(req,res)=>{
     res.send("thus is home");
     
+})
+app.get("/profile",(req,res,next)=>{
+    return next(new Error("this is error"));
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(500).send("something broke");
 })
 app.listen(3000);
