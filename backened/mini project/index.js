d98c04ba74cd8f317282aa4e3a2b246a2df733c8
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-08-04 16:15:41 +0500

    working on notes project

--- backened/mini project/index.js
+++ backened/mini project/index.js
@@ -1,33 +1,20 @@
-// const express =require('express');
-// const app = express();
-// const path = require("path");
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname,'public')));
-// app.set("view engine","ejs");
-// app.get("/",(req,res)=>{
-//     res.render("index");
-// })
-// app.listen(3000);
-
-const express = require('express');
-const app = express();
-const path=require("path"); 
+const express=require('express');
+const app=express();
+const port=3000; 
+const path=require('path');
+const fs=require('fs');
+app.set('view engine','ejs');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')))
-app.set("view engine","ejs");
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get("/",(req,res)=>{
-    res.render("index");
+app.get('/',(req,res)=>{
+    fs.readdir(`./files`,(err,files)=>{
+    res.render('index',{files:files});
+    });
 });
+app.post("/create",(req,res)=>{
+    console.log(req.body);
+})
 
-//dyanmic route
-app.get("/profile/:name",(req,res)=>{
-    res.send(`Hello, ${req.params.name}!`);
-});
-app.get("/profile/:name/:age",(req,res)=>{
-    res.send(`Hello, ${req.params.name}! You are ${req.params.age} years old.`);
-});
-app.listen(3000);
+app.listen(port);
