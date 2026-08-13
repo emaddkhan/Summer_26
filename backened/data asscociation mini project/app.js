e92f4b398a48ae4a2e68550bb21c8a7ed97fbb07
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-08-14 00:12:20 +0500

    making a project mini on data association protected routes

--- backened/data asscociation mini project/app.js
+++ backened/data asscociation mini project/app.js
@@ -17,6 +17,17 @@ app.use(express.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
     res.render("index");
 })
+app.get("/login",(req,res)=>{
+  res.render("login")
+})
+app.get("/profile",isLoggedIn,(req,res)=>{
+  console.log(req.user)
+  res.render("login")
+})
+app.get("/logout",(req,res)=>{
+  res.cookie("token","");
+  res.redirect("/login");
+})
 app.post("/register",async(req,res)=>{
     let {username,name,age,email,password}=req.body;
     let user =await userModel.findOne({email})
@@ -30,8 +41,36 @@ app.post("/register",async(req,res)=>{
             email,
             password:hash,
         })
-        res.send(createUser)
+        let token=jwt.sign({email:email,userId:createUser._id},"secretKey");
+        res.cookie("token",token)
+        res.send("registered")
       })
     })
 })
+
+app.post("/login",async(req,res)=>{
+    let {email,password}=req.body;
+    let user =await userModel.findOne({email})
+    if(!user) return res.status(500).send("Something went wrong")   
+    bcrypt.compare(password,user.password,(err,result)=>{
+       if(result){
+        let token=jwt.sign({email:email,userId:user._id},"secretKey");
+        res.cookie("token",token)
+        res.status(200).send("you can login")
+        
+      }else res.redirect("/login")
+    })
+})
 app.listen(port)
+
+
+function isLoggedIn(req,res,next){
+     if(req.cookies.token===""){
+       res.send("you must be looged in")
+     }else{
+      let data=jwt.verify(req.cookies.token,"secretKey")
+      req.user=data
+     next();
+
+     }
+}
