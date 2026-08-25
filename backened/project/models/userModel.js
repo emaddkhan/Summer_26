331999ba685205c8837cfb8c7211c2ef5e16b840
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-08-25 16:26:52 +0500

    workink on models

---
+++ backened/project/models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose =require("mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/ecommerce-project")
+
+const userSchema=mongoose.Schema({
+    fullname:String,
+    email:String,
+    password:String,
+    cart:[],
+    isadmin:Boolean ,
+    orders:[],
+    contact:Number,
+    picture:String,
+})
+module.exports = mongoose.model("user",userSchema);
