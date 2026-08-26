e0957ac7d2b6c129ec09dd7bcb910a0876bdbb8e
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-08-27 00:19:33 +0500

    setup mongoose-connetion and created and owners on new project

--- backened/project/config/mongoose-connection.js
+++ backened/project/config/mongoose-connection.js
@@ -1,12 +1,14 @@
 const { default: mongoose } = require("mongoose");
-
+const config=require("config")
+const dbgr=require("debug")("development:mongoose")
+// mongodb://127.0.0.1:27017/ecommerce-project
 mongoose
-.connect("mongodb://127.0.0.1:27017/ecommerce-project")
+.connect(`${config.get("MONGODB_URI")}/ecommerce-project`)
 .then(()=>{
-    console.log("connected")
+    dbgr("connected")
 })
 .catch((err)=>{
-    console.log(err)
+    dbgr(err)
 })
 
 module.exports=mongoose.connection
--- backened/project/routes/ownersRouter.js
+++ backened/project/routes/ownersRouter.js
@@ -1,8 +1,28 @@
 const express=require("express")
 const router=express.Router();
+const ownerModel=require("../models/ownersModel")
 
+
+if(process.env.NODE_ENV==="development"){
+    router.post("/create",async(req,res)=>{
+      let owner =await ownerModel.find();
+      if(owner.length>0) {
+          return res
+         .status(500)
+         .send("you are not allowed to create an owner")
+       }
+       let {fullname,email,password}=req.body;
+       let createdOwner=await ownerModel.create({
+        fullname,
+        email,
+        password
+       })
+       res.status(201 ).send(createdOwner)
+    })
+}
 router.get("/",(req,res)=>{
     res.send("heyyy")
 })
+
 
 module.exports=router;
