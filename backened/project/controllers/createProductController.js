3558c99ffdb8437b0069e6d38e6ea57410c3bc02
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-08-29 00:29:12 +0500

    made it more structurized

---
+++ backened/project/controllers/createProductController.js
@@ -0,0 +1,20 @@
+const productModel = require("../models/productModel");
+
+module.exports.createProduct=async(req, res) => {
+  try{
+    let { name, price, discount, bgColor, panelColor, textColor } = req.body;
+  let product = await productModel.create({
+    image: req.file.buffer,
+    name,
+    price,
+    discount,
+    bgColor,
+    panelColor,
+    textColor,
+  });
+  req.flash("success","product created")
+  res.redirect("/owners/admin")
+  }catch(err){
+    res.send(err.message)
+  }
+}
--- backened/project/routes/productsRouter.js
+++ backened/project/routes/productsRouter.js
@@ -1,25 +1,8 @@
 const express = require("express");
 const upload = require("../config/multer-config");
-const productModel = require("../models/productModel");
+const { createProduct } = require("../controllers/createProductController");
 const router = express.Router();
 
-router.post("/create", upload.single("image"), async (req, res) => {
-  try{
-    let { name, price, discount, bgColor, panelColor, textColor } = req.body;
-  let product = await productModel.create({
-    image: req.file.buffer,
-    name,
-    price,
-    discount,
-    bgColor,
-    panelColor,
-    textColor,
-  });
-  req.flash("success","product created")
-  res.redirect("/owners/admin")
-  }catch(err){
-    res.send(err.message)
-  }
-});
+router.post("/create", upload.single("image"),createProduct);
 
 module.exports = router;
