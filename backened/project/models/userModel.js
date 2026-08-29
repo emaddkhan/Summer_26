217b62a4b3655ecc47abaae44e918ce7b9d1ad08
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-08-30 01:07:17 +0500

    ecommerce project completed

--- backened/project/models/userModel.js
+++ backened/project/models/userModel.js
@@ -1,16 +1,28 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema=mongoose.Schema({
-    fullname:{
-        type:String,
-        minLength:3,
-        trim:true,
+const userSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    minLength: 3,
+    trim: true,
+  },
+  email: String,
+  password: String,
+  cart: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "product",
+      },
+      quantity:{
+        type:Number,
+        default:1,
+        min:1,
+      }
     },
-    email:String,
-    password:String,
-    cart:[],
-    orders:[],
-    contact:Number,
-    picture:String,
-})
-module.exports = mongoose.model("user",userSchema);
+  ],
+  orders: [],
+  contact: Number,
+  picture: String,
+});
+module.exports = mongoose.model("user", userSchema);
--- backened/project/routes/index.js
+++ backened/project/routes/index.js
@@ -1,25 +1,116 @@
-const express=require("express");
+const express = require("express");
 const { isLoggedIn } = require("../middleware/isLoggedIn");
 const productModel = require("../models/productModel");
-const router=express.Router();
+const userModel = require("../models/userModel");
 
-router.get("/",(req,res)=>{
-  let error=req.flash("error")
-  let success=req.flash("success")
-    res.render("index",{error,success,loggedin:false})
-})
-router.get("/shop",isLoggedIn,async(req,res)=>{
-  let products=await productModel.find();
-  let success=req.flash("success")
-  res.render("shop",{success,products})
-})
-router.get("/cart/:id",isLoggedIn,async(req,res)=>{
-  const product=await productModel.findById(req.params.id)
-  const user=await userModel.findOne({email:req.user.email})
-  user.cart.push(product._id)
-  await user.save();
-  console.log(user)
-})
+const router = express.Router();
 
 
-module.exports=router
+// HOME
+router.get("/", (req, res) => {
+
+    let error = req.flash("error");
+    let success = req.flash("success");
+
+    res.render("index", {
+        error,
+        success,
+        loggedin: false
+    });
+
+});
+
+
+// SHOP
+router.get("/shop", isLoggedIn, async (req, res) => {
+
+    let products = await productModel.find();
+
+    let success = req.flash("success");
+
+    res.render("shop", {
+        success,
+        products
+    });
+
+});
+
+
+// ADD TO CART
+router.get("/cart/:id", isLoggedIn, async (req, res) => {
+
+    const product = await productModel.findById(req.params.id);
+
+    const user = await userModel.findOne({
+        email: req.user.email
+    });
+
+    if (!product) {
+        req.flash("error", "Product not found");
+        return res.redirect("/shop");
+    }
+
+    if (!user) {
+        req.flash("error", "User not found");
+        return res.redirect("/");
+    }
+
+
+    // Check if product already exists
+    const existingProduct = user.cart.find(
+        item =>
+            item.product &&
+            item.product.toString() === product._id.toString()
+    );
+
+
+    if (existingProduct) {
+
+        // Product already exists
+        existingProduct.quantity += 1;
+
+    } else {
+
+        // New product
+        user.cart.push({
+            product: product._id,
+            quantity: 1
+        });
+
+    }
+
+
+    // Save in BOTH cases
+    await user.save();
+
+    req.flash("success", "Added to cart");
+
+    res.redirect("/shop");
+
+});
+
+
+// CART PAGE
+router.get("/cart", isLoggedIn, async (req, res) => {
+
+    const user = await userModel
+        .findOne({
+            email: req.user.email
+        })
+        .populate("cart.product");
+
+
+    if (!user) {
+        return res.redirect("/");
+    }
+
+
+
+    res.render("cart", {
+        cart:user.cart
+    });
+
+});
+
+
+module.exports = router;
