f7743f41f17546c38818252b670e61b288ee7bd8
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-09-12 23:42:49 +0500

    working HRMS hr dashboard worked on leaves and user

--- backened/HRMS/routes/hrRoute.js
+++ backened/HRMS/routes/hrRoute.js
@@ -7,10 +7,40 @@ const leaveModal=require("../models/leaveModel")
 
 router.get("/",isLoggedIn,async(req,res)=>{
     let users=await userModal.find({role:{$in:["employee","hr"]}});
-    console.log(users)
     let leaves=await leaveModal.find().populate("user");
 
     res.render("hrDashboard",{users,leaves})
+})
+
+router.get("/employees",async(req,res)=>{
+    let allStaff=await userModal.find({role:{$in:["employee","hr"]}})
+    let allEmp=await userModal.find({role:"employee"})
+    let allHr=await userModal.find({role:"hr"})
+    res.render("hrEmployees",{allStaff,allEmp,allHr})
+})
+router.get("/leaves",async(req,res)=>{
+    let leaves=await leaveModal.find().populate("user");
+    let successMessage=req.flash("success")
+    res.render("hrLeaves",{leaves,successMessage})
+})
+router.get("/user/:id",async(req,res)=>{
+    let user=await userModal.findById(req.params.id).populate("leaves.pendingLeaves").populate("leaves.approvedLeaves").populate("leaves.rejectedLeaves").populate("leaves.totalLeaves")
+    res.render("hrUserDetails",{user})
+})
+router.get("/leave/approve/:id",async(req,res)=>{
+    try{
+        let leave=await leaveModal.findById(req.params.id);
+    console.log(leave)
+    if(leave.status==="approved"){
+        req.flash("success","leaves is already approved")
+        return res.redirect("/hr/leaves")
+    }
+    leave.status="approved";
+    await leave.save()
+    res.redirect("/hr/leaves")
+    }catch(err){
+        console.log(err.message)
+    }
 })
 
 
